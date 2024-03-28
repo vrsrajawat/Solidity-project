235056513e41846b66b219838949482ff2713f66
vrsrajawat <155089430+vrsrajawat@users.noreply.github.com>
2024-03-28 22:05:47 +0530

    Update Project1.sol

--- Project1.sol
+++ Project1.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.18;
 
 contract MyToken {
    
-  // public variables here
-    string public tokenName = "META";
-    string public tokenAbbry = "HTA";
+  // public variables 
+    string public tokenName = "Varun";
+    string public tokenAbbry = "Var";
     uint public totalSupply = 0;
     
- // mapping variable here
+ // mapping variable 
     mapping(address => uint) public balances;
      
 // mint function here
@@ -17,7 +17,7 @@ contract MyToken {
        balances[_address] += _value;
     }
     
-// burn function here
+// burn function 
     function burn (address _address, uint _value) public {
        if (balances[_address] >= _value) {
           totalSupply -= _value;
